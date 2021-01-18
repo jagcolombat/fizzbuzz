935d012b5f2d328d1be9731232137dbe204fc781
tony <tonytunes2005@gmail.com>
2021-01-18 09:40:04 -0500

    setup project

---
+++ apps/front/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'realestate-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'FizzBuzz';
+}
